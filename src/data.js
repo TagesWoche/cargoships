1aae89585f421617b86a97323d819be4b53f25f4
Felix Michel <felix_michel@gmx.ch>
2018-06-21 17:04:06 +0200

    put data in separate file

---
+++ src/data.js
@@ -0,0 +1,142 @@
+const data2017 = [
+  {
+    'x': 41,
+    'y': 25,
+    'animation_time': 100
+  },
+  {
+    'x': 68,
+    'y': 25,
+    'animation_time': 200
+  },
+  {
+    'x': 95,
+    'y': 25,
+    'animation_time': 300
+  },
+  {
+    'x': 41,
+    'y': 8,
+    'animation_time': 400
+  },
+  {
+    'x': 68,
+    'y': 8,
+    'animation_time': 500
+  },
+  {
+    'x': 95,
+    'y': 8,
+    'animation_time': 600
+  },
+  {
+    'x': 41,
+    'y': -9,
+    'animation_time': 700
+  },
+  {
+    'x': 68,
+    'y': -9,
+    'animation_time': 800
+  },
+  {
+    'x': 95,
+    'y': -9,
+    'animation_time': 900
+  },
+  {
+    'x': 41,
+    'y': -26,
+    'animation_time': 1000
+  },
+  {
+    'x': 68,
+    'y': -26,
+    'animation_time': 1100
+  },
+  {
+    'x': 95,
+    'y': -26,
+    'animation_time': 1200
+  },
+  {
+    'x': 41,
+    'y': -43,
+    'animation_time': 1300
+  },
+  {
+    'x': 68,
+    'y': -43,
+    'animation_time': 1400
+  },
+  {
+    'x': 95,
+    'y': -43,
+    'animation_time': 1500
+  },
+  {
+    'x': 41,
+    'y': -60,
+    'animation_time': 1600
+  },
+  {
+    'x': 68,
+    'y': -60,
+    'animation_time': 1700
+  },
+  {
+    'x': 95,
+    'y': -60,
+    'animation_time': 1800
+  },
+  {
+    'x': 41,
+    'y': -77,
+    'animation_time': 1900
+  },
+  {
+    'x': 68,
+    'y': -77,
+    'animation_time': 2000
+  },
+  {
+    'x': 95,
+    'y': -77,
+    'animation_time': 2100
+  },
+  {
+    'x': 41,
+    'y': -94,
+    'animation_time': 2200
+  },
+  {
+    'x': 68,
+    'y': -94,
+    'animation_time': 2300
+  },
+  {
+    'x': 95,
+    'y': -94,
+    'animation_time': 2400
+  }
+]
+
+const data1987 = [
+  {
+    'x': 41,
+    'y': 25,
+    'animation_time': 100
+  },
+  {
+    'x': 68,
+    'y': 25,
+    'animation_time': 200
+  },
+  {
+    'x': 95,
+    'y': 25,
+    'animation_time': 300
+  }
+]
+
+export { data2017, data1987 }
